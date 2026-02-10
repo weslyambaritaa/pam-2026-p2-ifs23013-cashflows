e9689eb745127cce342d62ba05134433eee3b8aa
wesly <weslyambarita4@gmail.com>
2026-02-10 13:54:26 +0700

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,11 @@ val kotlin_version: String by project
 val logback_version: String by project
 val dotenv_version: String by project
 
+
 plugins {
     kotlin("jvm") version "2.3.0"
     id("io.ktor.plugin") version "3.3.2"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.0"
 }
 
 group = "org.delcom"
